2536c74d1849d93b87d0374bda2a0a31446252c8
kriss <462679766@qq.com>
2025-07-18 16:35:05 +0800

    feature: add ResponsePropertyIn

---
+++ src/Attributes/ResponsePropertyInJson.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace WebmanTech\DTO\Attributes;
+
+use Attribute;
+use WebmanTech\DTO\Enums\RequestPropertyInEnum;
+
+#[Attribute(Attribute::TARGET_PROPERTY | Attribute::TARGET_PARAMETER)]
+final class ResponsePropertyInJson extends RequestPropertyIn
+{
+    public function __construct(
+        public ?string $name = null,
+    )
+    {
+        parent::__construct(RequestPropertyInEnum::Json->value, name: $this->name);
+    }
+}
