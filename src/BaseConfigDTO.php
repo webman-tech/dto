938996fcdadf081bf082a6120721b1553464899c
kriss <462679766@qq.com>
2025-07-29 10:36:18 +0800

    feature: BaseConfigDTO 默认不进行验证

--- src/BaseConfigDTO.php
+++ src/BaseConfigDTO.php
@@ -6,8 +6,11 @@ use WebmanTech\DTO\Helper\ArrayHelper;
 
 class BaseConfigDTO extends BaseDTO
 {
-    public static function fromConfig(array|BaseConfigDTO $config = [], bool $validate = true): static
+    public static function fromConfig(array|BaseConfigDTO $config = [], bool $validate = false): static
     {
+        // $validate 默认为 false 的原因：
+        // 1. config 一般是服务端自己的配置，一般不需要校验，增加校验损耗性能
+        // 2. validate 后只会返回验证后的数据，当 config 中有些配置是在 construct 中定义了，但是没有 public 属性，此时会丢失，参见 WebmanTech\Swagger\DTO\ConfigRegisterRouteDTO
         if ($config instanceof static) {
             return $config;
         }
